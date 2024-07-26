9df9ac2d66f19d6759b78e53d32e6023f28e691c
mraihanaf <mraihanaf0@gmail.com>
2024-07-26 23:43:13 +0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,40 @@
+import makeWASocket, { DisconnectReason, BufferJSON, useMultiFileAuthState } from '@whiskeysockets/baileys'
+import { Boom } from '@hapi/boom'
+import pino from 'pino'
+
+const logger = pino({
+    name: "casisbot",
+    level: "debug"
+})
+
+async function connectToWhatsapp() {
+    logger.info("connecting to whatsapp")
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info')
+    const sock = makeWASocket({
+        printQRInTerminal: true,
+        auth: state,
+        // @ts-ignore
+        logger: logger
+    })
+    sock.ev.on('creds.update', saveCreds)
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update
+        if(connection === 'close') {
+            if(!lastDisconnect) return console.log("last disconnect undefined")
+            const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
+            logger.error('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+            // reconnect if not logged out
+            if(shouldReconnect) {
+                logger.info("reconnecting to whatsapp")
+                connectToWhatsapp()
+            }
+        } else if(connection === 'open') {
+            logger.info('opened connection')
+        }
+    })
+
+}
+
+connectToWhatsapp().catch(err => {
+    console.error(err)
+})
